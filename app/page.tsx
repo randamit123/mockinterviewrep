9db14f58822978d8220b19ce22d5ad138c9036a6
randamit123 <randamit123@gmail.com>
2023-06-18 13:16:43 -0700

    Merging

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,7 @@
 
 import { ChangeEvent, useState } from "react";
 
-import Image from "next/image"
+import Image from "next/image";
 import Link from "next/link";
 
 export default function Home() {
@@ -13,43 +13,39 @@ export default function Home() {
   };
 
   return (
-    <div className='flex flex-col justify-center h-screen'>
-      <div className='my-auto'>
-        <Image
-          className="mx-auto"
-          alt="logo"
-          src="/logo.svg"
-          height={134}
-          width={363}
-          priority
-        />
-        <div className='mx-auto mt-8 text-6xl font-bold text-center'>
-          Welcome!
-        </div>
-        <div className='w-80 mx-auto mt-16'>
-          <input
-            type='text'
-            placeholder='Your first name'
-            className='h-16 w-full outline outline-1 outline-black outline-offset-0 p-5'
-            onChange={onNameChange}
+    <div className="flex flex-col p-4">
+      <div>
+        <nav className="">
+          <Image
+            alt="logo"
+            src="/logo.svg"
+            height={134 * 0.7}
+            width={363 * 0.7}
+            priority
           />
-          <select className='h-16 w-full outline outline-1 outline-black outline-offset-0 mt-8 p-5'>
-            <option value="javascript">SWE Interview</option>
-            <option value="php" disabled>PM Interview (coming soon)</option>
-            <option value="java" disabled>Consulting Interview (coming soon)</option>
-            <option value="golang" disabled>Banking interview (coming soon)</option>
-          </select>
-          <Link
-            className={`
-              w-full mt-8 bg-gray-400 text-white text-3xl p-4 block
-              text-center ${name !== "" ? "" : "pointer-events-none"}
-            `}
-            href={`/interview?name=${name}`}
-          >
-            Get started!
-          </Link>
-        </div>
+        </nav>
       </div>
+      <main className="grid w-full mx-auto min-h-[80vh]">
+        <div className="flex gap-8 flex-col items-center justify-center">
+          <h1 className="lg:pt-28 text-5xl md:text-6xl lg:text-8xl font-semibold text-center max-w-[18ch]">
+            Never Fail Another Interview Again
+          </h1>
+          <p className="text-xl md:text-2xl lg:text-3xl max-w-[28ch] text-center text-gray-600">
+            Practice with our AI interviewers, get feedback and excel
+          </p>
+          <div
+            className="button block"
+            data-btn-intent="primary"
+            data-btn-size="large"
+          >
+            <Link href="/interview" className="text-xl">
+              Interview Now
+            </Link>
+          </div>
+
+          <div className="w-full max-w-[1200px] aspect-video bg-gray-200 rounded-xl"></div>
+        </div>
+      </main>
     </div>
-  )
+  );
 }
